3c408abf5c72eb71943ab79299ed4576262f59d1
SushantVarian <sushant.sardeshpande@varian.com>
2024-07-22 16:00:32 +0530

    Base

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-[![Build Status](https://dev.azure.com/firely/firely-net-sdk/_apis/build/status/FirelyTeam.firely-net-sdk?branchName=develop)](https://dev.azure.com/firely/firely-net-sdk/_build/latest?definitionId=84&branchName=develop)
-
-> **IMPORTANT** The 5.0 version of the SDK contains substantial changes to the way we have organized the NuGet packages and source code. Please read before installing this new 5.0 version.
+|STU3|R4|R4B|R5|
+|---|---|---|---|
+|[![Build Status](https://dev.azure.com/firely/firely-net-sdk/_apis/build/status/FirelyTeam.firely-net-sdk?branchName=develop-stu3)](https://dev.azure.com/firely/firely-net-sdk/_build/latest?definitionId=84&branchName=develop-stu3)|[![Build Status](https://dev.azure.com/firely/firely-net-sdk/_apis/build/status/FirelyTeam.firely-net-sdk?branchName=develop-r4)](https://dev.azure.com/firely/firely-net-sdk/_build/latest?definitionId=84&branchName=develop-r4)|[![Build Status](https://dev.azure.com/firely/firely-net-sdk/_apis/build/status/FirelyTeam.firely-net-sdk?branchName=develop-r4B)](https://dev.azure.com/firely/firely-net-sdk/_build/latest?definitionId=84&branchName=develop-r4B)|[![Build Status](https://dev.azure.com/firely/firely-net-sdk/_apis/build/status/FirelyTeam.firely-net-sdk?branchName=develop-r5)](https://dev.azure.com/firely/firely-net-sdk/_build/latest?definitionId=84&branchName=develop-r5)|
+| [![Nuget](https://img.shields.io/nuget/dt/Hl7.Fhir.STU3)](https://www.nuget.org/packages/Hl7.Fhir.STU3) |[![Nuget](https://img.shields.io/nuget/dt/Hl7.Fhir.R4)](https://www.nuget.org/packages/Hl7.Fhir.R4) | [![Nuget](https://img.shields.io/nuget/dt/Hl7.Fhir.R4B)](https://www.nuget.org/packages/Hl7.Fhir.R4B)|[![Nuget](https://img.shields.io/nuget/dt/Hl7.Fhir.R5)](https://www.nuget.org/packages/Hl7.Fhir.R5) |
 
 ## Introduction ##
-This is Firely's official support SDK for working with [HL7 FHIR][fhir-spec] on the Microsoft .NET (dotnet) platform.
+This is the official support SDK for working with [HL7 FHIR][fhir-spec] on the Microsoft .NET (dotnet) platform.
 
-This SDK provides:
+## Release notes ##
+Read the releases notes on [firely-net-sdk/releases](https://github.com/FirelyTeam/firely-net-sdk/releases). You can find documentation about this SDK in [the Firely docs site][netsdk-docu].
+
+## What's in the box?
+This library provides:
 * Class models for working with the FHIR data model using POCO's
 * Xml and Json parsers and serializers
 * A REST client for working with FHIR-compliant servers
@@ -13,67 +18,48 @@ This SDK provides:
 * Validation of instances based on profiles
 * Evaluation [FhirPath][fhirpath-spec] expressions
 
-## Release notes ##
-Read the releases notes on [firely-net-sdk/releases](https://github.com/FirelyTeam/firely-net-sdk/releases). You can find documentation about this SDK in [the Firely docs site][netsdk-docu].
-
-
-## Getting Started ##
+**IMPORTANT**
 Before installing one of the NuGet packages (or clone the repo) it is important to understand that HL7 has published several updates of the FHIR specification, each with breaking changes - so you need to ensure you use the version that is right for you:
 
-* [STU3][stu3-spec] (published March 2017) is older, but still widely in use and fully supported by the SDK.
-* [R4][r4-spec] (published January 2019) is in active use and fully supported by the SDK.
-* [R4B][r4B-spec] (published May 2022) is in active use and fully supported by the SDK.
-* [R5][r5-spec] (published March 26 2023) is the latest official release of the FHIR spec and is fully supported by the SDK.
+* [DSTU1][dstu1-spec] (published September 2014) is mostly obsolete, and the .NET version for this publication is not maintained anymore.
+* [DSTU2][dstu2-spec] (published October 2015) in widespread use, and is not supported by this library anymore since version 2.0.
+* [STU3][stu3-spec] (published March 2017), mature support by this library and most other tooling on the market.
+* [R4][r4-spec] (published January 2019), mature support by this library and fully supported by this library.
+* [R4B][r4B-spec] (published May 2022), the latest release of the FHIR spec and fully supported by this library.
 
-Read the [online documentation][netsdk-docu], and download the correct for your FHIR release. Depending on the version of FHIR you require, you'll find the relevant link to the package below. For most developers, just including this NuGet package is enough to get started. 
+Planned release DSTU2.1 was never published by HL7, but you will still find traces of it, in particular we still keep the NuGet package for it available.
 
-|Spec version|Git branch| NuGet|
+## Getting Started ##
+Get started by reading the [online documentation][netsdk-docu]. Depending on the version of FHIR you require, you'll find the relevant links to the packages
+and develop branches in this repository below:
+
+|Spec version|Git branch|Core NuGet|
 |---|---|---|
-|R5 | https://github.com/FirelyTeam/firely-net-sdk/tree/release/5.0.0 | https://www.nuget.org/packages/Hl7.Fhir.R5 |
-|R4B| https://github.com/FirelyTeam/firely-net-sdk/tree/release/5.0.0 | https://www.nuget.org/packages/Hl7.Fhir.R4B |
-|R4| https://github.com/FirelyTeam/firely-net-sdk/tree/release/5.0.0 | https://www.nuget.org/packages/Hl7.Fhir.R4 | 
-|STU3| https://github.com/FirelyTeam/firely-net-sdk/tree/release/5.0.0 | https://www.nuget.org/packages/Hl7.Fhir.STU3 | 
+|R5 (experimental)| https://github.com/FirelyTeam/firely-net-sdk/tree/develop-r5 | https://www.nuget.org/packages/Hl7.Fhir.R5 |
+|R4B| https://github.com/FirelyTeam/firely-net-sdk/tree/develop-r4B | https://www.nuget.org/packages/Hl7.Fhir.R4B |
+|R4| https://github.com/FirelyTeam/firely-net-sdk/tree/develop-r4 | https://www.nuget.org/packages/Hl7.Fhir.R4 | 
+|STU3| https://github.com/FirelyTeam/firely-net-sdk/tree/develop-stu3 | https://www.nuget.org/packages/Hl7.Fhir.STU3 | 
+|DSTU2| https://github.com/FirelyTeam/firely-net-sdk/tree/develop | https://www.nuget.org/packages/Hl7.Fhir.DSTU2 | 
+|DSTU1| https://github.com/FirelyTeam/firely-net-sdk/tree/master-dstu1 | https://www.nuget.org/packages/Hl7.Fhir.DSTU | 
 
-### Using a pre-release NuGet package
-Every release of the SDK results in a NuGet package on the normal NuGet feed. However, each commit on our develop branch also results in a pre-release package.
-These are public too. So if you want to be brave and use a pre-release packages, you can do so by adding ```https://nuget.pkg.github.com/FirelyTeam/index.json``` to your NuGet sources:
+Please note that the source code for the Firely .NET SDK is split up into two GitHub repos: one (with the branches listed above) with code that is specific to a FHIR release (this repo), and one that contains the code that is applicable across all FHIR releases (a separate [common repository][common-repo]).  This second repository is included in the first one using a Git Submodule, so all you have to do is check out the main repo (this one) and learn how to work with Submodules. To clone this repository with submodules, you can do:
 
-- Get a Personal Access token (PAT) from [github.com][github-pat] with scope ```read:packages```
-
-- Next open a console on your machine and run ```dotnet nuget add source --name github --username <USERNAME> --password <PAT> https://nuget.pkg.github.com/FirelyTeam/index.json```
-
-```USERNAME```: your username on GitHub
-```PAT```: your Personal access token with at least the scope ```read:packages```
-
+    git clone --recurse-submodules -j8 https://github.com/FirelyTeam/firely-net-sdk.git .
+    
+Please refer to our [submodules overview](https://github.com/FirelyTeam/firely-net-sdk/wiki/Clone-this-repository-with-submodule-common) for more details.
 
 ## Upgrading
-We spend a lot of effort trying to maintain *compile* compatibility (not binary compatibility) between minor releases of the SDK. We do, however, publish a new major version with breaking changes about once a year. The table below lists the breaking changes for each of the major upgrades.
+Upgrading to 2.x? Breaking changes are [listed here](https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-2.0).
 
-|SDK version|Breaking changes
-|---|---|
-|2.x|https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-2.0|
-|3.x|https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-3.0|
-|4.x|https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-4.0|
-|5.x|https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-5.0|
-
-The SDK has been restructured for the 5.0 release. Please take note of the following changes if you are upgrading:
-* You should only reference the main package (`Hl7.Fhir.<release>`). 
-* If you need the `specification.zip` (for validation, if you are using the `ZipSource` resolver), add `Hl7.Fhir.Specification.Data.<release>`.
-* The "old" `Hl7.Fhir.Specification.<release>` package is now a metapackage that will include these two packages.
-* You should not reference any other packages that existed pre-5.0 (`Hl7.Fhir.ElementModel` etc.)
-
-The profile validator has been split off into its own [repository](https://github.com/FirelyTeam/firely-validator-api). The NuGet packages for the validator that are compatible with the SDK 5.0 release can be found on [NuGet](https://www.nuget.org/packages?q=Hl7.Fhir.Validation.Legacy).
+Going further? See [3.x breaking changes](https://github.com/FirelyTeam/firely-net-sdk/wiki/Breaking-changes-in-3.0).
 
 ## Support 
 We actively monitor the issues coming in through the GitHub repository at [https://github.com/FirelyTeam/firely-net-sdk/issues](https://github.com/FirelyTeam/firely-net-sdk/issues). You are welcome to register your bugs and feature suggestions there. For questions and broader discussions, we use the .NET FHIR Implementers chat on [Zulip][netsdk-zulip].
 
 ## Contributing ##
-We are welcoming contributions!
+We are welcoming contributors!
 
-If you want to participate in this project, we're using [Git Flow][nvie] for our branch management. Please submit PRs with changes against the `develop` branche.
-
-> Note: Since the 5.0 release of the SDK, the branches for STU3 and newer have been combined in a single `develop` branch. This branch now contains the code for all FHIR releases from STU3 and up. We have also refactored all the common code out to projects within that branch, so the separate `common` repository (at https://github.com/FirelyTeam/firely-net-common) is no longer in use. This greatly simplifies management and creating PRs for these projects.
-
+If you want to participate in this project, we're using [Git Flow][nvie] for our branch management, so please submit your commits using pull requests on the correct `develop-stu3`/`develop-r4`/`develop-r4B`/`develop-r5` branches as mentioned above! 
 
 [common-repo]: https://github.com/FirelyTeam/firely-net-common
 [netsdk-docu]: https://docs.fire.ly/projects/Firely-NET-SDK/
@@ -85,6 +71,8 @@ If you want to participate in this project, we're using [Git Flow][nvie] for our
 [stu3-spec]: http://www.hl7.org/fhir/stu3
 [r4-spec]: http://hl7.org/fhir/R4/
 [r4B-spec]: http://hl7.org/fhir/index.html
-[r5-spec]: http://build.fhir.org/index.html
 [fhirpath-spec]: http://hl7.org/fhirpath/
-[github-pat]: https://github.com/settings/apps
+
+### GIT branching strategy 
+- [NVIE](http://nvie.com/posts/a-successful-git-branching-model/)
+- Or see: [Git workflow](https://www.atlassian.com/git/workflows#!workflow-gitflow)
--- build/DownloadFhirBuildProfile.ps1
+++ build/DownloadFhirBuildProfile.ps1
@@ -4,39 +4,25 @@
 # cls
 
 # Script to be run from 'build' directory
-using namespace System.Collections.Generic
 
-param(
-     [Parameter(Mandatory, HelpMessage="Enter the url 	from which the specification is to be downloaded")]
-     [string]$server = "http://hl7.org/fhir/STU3/",
-	 
-	 [ValidateSet('STU3', 'R4', 'R4B', 'R5')]
-     [Parameter(Mandatory, HelpMessage="Enter the Fhir Release ('STU3', 'R4', 'R4B' or 'R5')")]
-     [string] $fhirRelease = "STU3" 
-)
-
+$server = "http://hl7.org/fhir/";
 $baseDir = Resolve-Path ..
 $srcdir = "$baseDir\src";
 
+
 # These are all files from the spec that we need. Narratives are stripped after download
-$allFiles = [List[string]]@(
-				"conceptmaps.xml",
+$allFiles = @("conceptmaps.xml",				
 				"extension-definitions.xml", 
 				"namingsystem-registry.xml",
 				"profiles-others.xml", 
 				"profiles-resources.xml", 
 				"profiles-types.xml" 
 				"search-parameters.xml",
+				"v2-tables.xml",
+				"v3-codesystems.xml",
 				"valuesets.xml"
 				"fhir-all-xsd.zip"
 				);
-
-if ($fhirRelease -eq "R5")
-{
-	# remove extension-definitions.xml and namingsystem-registry.xml because from R5 those files do not exist anymore.
-	$allFiles.Remove("extension-definitions.xml"); #
-	$allFiles.Remove("namingsystem-registry.xml"); #
-}
 
 function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
@@ -46,6 +32,7 @@ function New-TemporaryDirectory {
 }
 
 $tempDir = New-TemporaryDirectory
+
 
 # Download a file from a URL and place it at the specified target path.
 # This also has some basic capabilities to go through proxies without any additional configuration.
@@ -69,6 +56,18 @@ function GetSpecFile($name)
 	$targetPath = Join-Path $tempDir $name
 	$sourceUrl = $server + $name
 	PowerCurl $targetPath $sourceUrl
+}
+
+
+function TransformSpecfile($name)
+{
+	Write-Host -ForegroundColor White "transforming $name..."
+	$transformPath = Join-Path $tempDir $name
+	$tempTransformPath = $transformPath + "-temp"
+
+	$xslt.Transform($transformPath, $tempTransformPath);
+	Remove-Item $transformPath
+	Move-Item $tempTransformPath $transformPath
 }
 
 function CopySpecFile($name, $destDir)
@@ -133,201 +132,23 @@ function RemoveNarrative($name)
 	$xml.Save($file)
 }
 
-function RemoveDoubleVersionsInValueSets($name)
-{
-	if ($server.EndsWith('2021May/') )
-	{
-		# Correction for R5, 4.6.0:
-		# Removed double version '|4.6.0|4.6.0' from <valueSet> 
-		
-		$filename = Join-Path $tempDir $name
-		[xml]$xml = Get-Content $filename
-		
-		$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-		$ns.AddNamespace("ns", "http://hl7.org/fhir") 
-		
-		$valuesets = $xml.SelectNodes('//ns:valueSet[substring(@value,string-length(@value) - string-length("|4.6.0|4.6.0") + 1) = "|4.6.0|4.6.0"]', $ns)
-		
-		foreach ($valueset in $valuesets)
-		{
-			$valueAttribute = $valueset.GetAttribute('value')
-			$valueAttribute = $valueAttribute.Replace("|4.6.0|", "|");
-			$valueset.SetAttribute('value', $valueAttribute)  | Out-null
-		}
-		
-		Write-Output "Removed double version '|4.6.0|4.6.0' from '<valueSet> $file"
-		$xml.Save($filename)
-	}
-}
-
-function ChangeValueElement($name)
-{
-	# Correction for 4B:
-	# changes Element "valueUri" to "valueUrl" of extension `structuredefinition-fhir-type`
-
-	$filename = Join-Path $tempDir $name
-	[xml]$xml = Get-Content $filename
-	
-	$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-	$ns.AddNamespace("ns", "http://hl7.org/fhir")
-
-	$extensionNodes = $xml.SelectNodes('//ns:extension[@url="http://hl7.org/fhir/StructureDefinition/structuredefinition-fhir-type"]', $ns)
-	
-	foreach ($extension in $extensionNodes)
-	{
-		$valueElement = $extension.FirstChild
-		$correctedValueElement = $xml.CreateElement("valueUrl", $ns.LookupNamespace("ns"))
-		$correctedValueElement.SetAttribute("value", $valueElement.Value) | Out-null
-		$extension.ReplaceChild($correctedValueElement, $valueElement)  | Out-null
-	}
-
-	Write-Output "Changed 'valueUri' to 'valueUrl' for extension 'structuredefinition-fhir-type' from $file"
-	$xml.Save($filename)
-}
-
-
-function RemoveDefinitonExtension($name)
-{
-	# Correction for 4B:
-	# remove the extension http://hl7.org/fhir/build/StructureDefinition/definition
-	
-	$filename = Join-Path $tempDir $name
-	[xml]$xml = Get-Content $filename
-	
-	$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-	$ns.AddNamespace("ns", "http://hl7.org/fhir")
-
-	$extensionNodes = $xml.SelectNodes('//ns:extension[@url="http://hl7.org/fhir/build/StructureDefinition/definition"]', $ns)
-	
-	foreach ($extension in $extensionNodes)
-	{
-		[void]$extension.ParentNode.RemoveChild($extension)
-	}
-	
-	Write-Output "Removed the extension http://hl7.org/fhir/build/StructureDefinition/definition from $file"
-	
-	$xml.Save($filename)
-}
-
-
 function ExtractXsdZipFile($destPath)
 {
 	Write-Host -ForegroundColor White "Extract xsd zip file..."
 	$zipPath = Join-Path $tempDir "fhir-all-xsd.zip"
 	$extractPath = Join-Path $tempDir "extracted"
-	Expand-Archive -path $zipPath -destinationpath $extractPath
+	expand-archive -path $zipPath -destinationpath $extractPath
 	
-	if ($server.EndsWith('2020Sep/') -or $server.EndsWith('2021Mar/') )
+	if ($server.EndsWith('2020Sep/') )
 	{
 		# In release 2020Sep is an error in the fhir-single.xsd.  
-		Write-Host -ForegroundColor White ".. corrected errors in fhir-single.xsd"
+		Write-Host -ForegroundColor White ".. correct errors in fhir-single.xsd"
 		$xsdFile = Join-Path $extractPath "fhir-single.xsd"
 		RemoveIncorrectXsdElements $xsdFile
 	}
 	Write-Host -ForegroundColor White "Copy extracted files to $destPath ..."
 	Copy-Item -Path $extractPath\* -Recurse -Container:$false -Destination $destPath
 }
-
-function ChangeValueElementOfFhirType($name)
-{
-	# Correction for R5 (5.0.0-snapshot1):
-	# Change type of value[x] of the extension structuredefinition-fhir-type to url
-	
-	$filename = Join-Path $tempDir $name
-	[xml]$xml = Get-Content $filename
-	
-	$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-	$ns.AddNamespace("ns", "http://hl7.org/fhir")
-
-	$typeNodes = $xml.SelectNodes('//ns:StructureDefinition[ns:id[@value = "structuredefinition-fhir-type"]]//ns:element[@id = "Extension.value[x]"]/ns:type', $ns)
-	
-	foreach ($typeNode in $typeNodes)
-	{
-		$codeElement = $typeNode.FirstChild
-		$correctedCodeElement = $xml.CreateElement("code", $ns.LookupNamespace("ns"))
-		$correctedCodeElement.SetAttribute("value", "url") | Out-null
-		$typeNode.ReplaceChild($correctedCodeElement, $codeElement)  | Out-null
-	}
-	
-	Write-Output "Changed type of 'value[x]' of structuredefinition-fhir-type from 'uri' to 'url'"
-	
-	$xml.Save($filename)
-}
-
-function CorrectConceptmap($name)
-{
-	# Correction for R4B (4.3.0-snapshot1):
-	# Change node <relationship> to <equivalence> for resource ConceptMap
-	
-	if (!$name.EndsWith('.xml'))
-	{
-		return;
-	}
-	
-	$filename = Join-Path $tempDir $name
-	[xml]$xml = Get-Content $filename
-	
-	$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-	$ns.AddNamespace("ns", "http://hl7.org/fhir")
-
-	$relationships = $xml.SelectNodes('//ns:ConceptMap/ns:group/ns:element/ns:target/ns:relationship', $ns)
-	
-	foreach ($relationship in $relationships)
-	{
-		$correctedElement = $xml.CreateElement("equivalence", $ns.LookupNamespace("ns"))
-		
-		$equivalenceValue = $relationship.Value
-		if ($equivalenceValue -eq "source-is-broader-than-target")
-		{
-			$equivalenceValue = "wider"
-		}
-		if ($equivalenceValue -eq "source-is-narrower-than-target")
-		{
-			$equivalenceValue = "narrower"
-		}
-		$correctedElement.SetAttribute("value", $equivalenceValue) | Out-null
-		
-		$childNodes = $relationship.ChildNodes;
-		
-		foreach ($childNode in $childNodes)
-		{
-			$newChild = $childNode.CloneNode("False")
-			$correctedElement.AppendChild($newChild)| Out-null
-		}
-		
-		$relationship.ParentNode.AppendChild($correctedElement) | Out-null
-        $relationship.ParentNode.RemoveChild($relationship) | Out-null
-	}
-	
-	Write-Output "Change node <relationship> to <equivalence> for resource ConceptMap"
-	
-	$xml.Save($filename)
-}
-
-function ExtractPackageToDirectory($packageBaseUrl, $packageName, $packageVersion, $destDir)
-{
-	$packageUrl = $packageBaseUrl + $packageName + "/" + $packageVersion;
-
-	$destFile = Join-Path $tempDir $packageName
-	$destFile = $destFile + ".tgz";
-
-	$extractDir = Join-Path $tempDir $packageName
-
-	# Download extension package
-	PowerCurl $destFile $packageUrl
-	
-	# Unpack extension package
-	New-Item -ItemType Directory $extractDir -Force | Out-Null
-	tar -xf $destFile -C $extractDir
-
-	# move part of the content to specification dir
-	New-Item -ItemType Directory $destDir -Force | Out-Null
-	$exclude = @('.index.json','package.json')
-	Copy-Item -Path $extractDir\Package\* -Filter "*.json" -Container:$false -Destination  $destDir  -Exclude $exclude 
-}
-
-# Start of processing:
-
 
 foreach($file in $allFiles)			
 {
@@ -336,82 +157,33 @@ foreach($file in $allFiles)
 	{
 		RemoveNarrative $file
 	}
-	
-	# Corrections for 4B:
-	if ($server.EndsWith("2021Mar/"))
-	{
-		if ($file.EndsWith('.xml'))
-		{
-			RemoveDefinitonExtension $file
-			ChangeValueElement $file
-		}
-	}
-	
-	# Corrections for R4B (4.3.0-snapshot1)
-	if ($server.EndsWith("4.3.0-snapshot1/"))
-	{
-		CorrectConceptmap $file
-	}
-	
-	# Corrections for R5 (4.6.0)
-	if ($server.EndsWith("2021May/"))
-	{
-		if ($file.EndsWith('profiles-resources.xml'))
-		{
-			RemoveDoubleVersionsInValueSets $file
-			RemoveDefinitonExtension $file
-		}
-	}
-	
-	# Corrections for R5 (5.0.0-snapshot1)
-	if ($server.EndsWith("5.0.0-snapshot1/"))
-	{
-		if ($file.EndsWith('extension-definitions.xml'))
-		{
-			ChangeValueElementOfFhirType $file
-		}
-	}
 }
-
-$specificationDir = New-TemporaryDirectory
 
 Write-Host -ForegroundColor White "Copy files to project..."
 
 # Copy the files necessary for the specification library (specification.zip / data)
-foreach($specFile in $allFiles)			
-{
-	if ($specFile.EndsWith(".xml")) 
-	{
-		Write-Host "Copy $specFile to $specificationDir"
-		CopySpecFile $specFile $specificationDir
-	}
-}
-
-if ($fhirRelease -eq "R5")
-{
-	# download package extensions and content to the specification.zip
-	$extensionDir = Join-Path $specificationDir "extensions"
-	ExtractPackageToDirectory -packageBaseUrl "http://packages2.fhir.org/packages/" -packageName "hl7.fhir.uv.extensions.r5" -packageVersion "1.0.0" -destDir $extensionDir;
-}
+Remove-Item "$srcdir\Hl7.Fhir.Specification\data\*.*" -Force
+CopySpecFile "conceptmaps.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "extension-definitions.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "namingsystem-registry.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "profiles-others.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "profiles-resources.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "profiles-types.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "search-parameters.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "v2-tables.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "v3-codesystems.xml" "$srcdir\Hl7.Fhir.Specification\data"
+CopySpecFile "valuesets.xml" "$srcdir\Hl7.Fhir.Specification\data"
 
 # Add example files used for testing
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\careplan-example-f201-renal.xml" "$server/careplan-example-f201-renal.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\testscript-example(example).xml" "$server/testscript-example.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\valueset-v2-0717.json" "$server/v2/0717/v2-0717.vs.json"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples.zip" "$server/examples.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples-json.zip" "$server/examples-json.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
+PowerCurl "$srcdir\Hl7.Fhir.Serialization.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
+PowerCurl "$srcdir\Hl7.Fhir.Specification.Tests\TestData\careplan-example-integrated.xml" "$server/careplan-example-integrated.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Specification.Tests\TestData\profiles-types.json" "$server/profiles-types.json"
 
-if ($fhirRelease -eq "STU3") 
-{
-	PowerCurl "$srcdir\Hl7.Fhir.$fhirRelease.Tests\TestData\careplan-example-f201-renal.xml" "$server/careplan-example-f201-renal.xml"
-	PowerCurl "$srcdir\Hl7.Fhir.$fhirRelease.Tests\TestData\testscript-example(example).xml" "$server/testscript-example.xml"
-	PowerCurl "$srcdir\Hl7.Fhir.Serialization.$fhirRelease.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
-}
-
-PowerCurl "$srcdir\Hl7.Fhir.$fhirRelease.Tests\TestData\examples.zip" "$server/examples.zip"
-PowerCurl "$srcdir\Hl7.Fhir.$fhirRelease.Tests\TestData\examples-json.zip" "$server/examples-json.zip"
-PowerCurl "$srcdir\Hl7.Fhir.$fhirRelease.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
-
-PowerCurl "$srcdir\Hl7.Fhir.Specification.$fhirRelease.Tests\TestData\careplan-example-integrated.xml" "$server/careplan-example-integrated.xml"
-PowerCurl "$srcdir\Hl7.Fhir.Specification.$fhirRelease.Tests\TestData\profiles-types.json" "$server/profiles-types.json"
-PowerCurl "$srcdir\Hl7.Fhir.Specification.$fhirRelease.Tests\TestData\snapshot-test\profiles-resources.xml" "$server/profiles-resources.xml"
-
-# extract schemas and xsd from fhir-all.zip -> temp specification directory
-ExtractXsdZipFile $specificationDir
-
-Compress-Archive -Path $specificationDir\* -DestinationPath $srcdir\Hl7.Fhir.Specification.Data.$fhirRelease\specification.zip -Force
+# extract schemas and xsd from fhir-all.zip -> data
+ExtractXsdZipFile "$srcdir\Hl7.Fhir.Specification\data"
